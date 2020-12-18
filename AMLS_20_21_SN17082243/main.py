7ef20d92a9023e50e409236f7f00e5c056e957c5
98Rico <69646032+98Rico@users.noreply.github.com>
2020-12-18 19:12:17 +0000

    Update main.py

--- AMLS_20_21_SN17082243/main.py
+++ AMLS_20_21_SN17082243/main.py
@@ -1,1 +1,41 @@
+# Viasualisation of the data labels
+
+import pandas as pd
+
+## Celeba labels2.csv data
+
+celeba_df = pd.read_csv('./datasets/celeba/labels2.csv')
+celeba_df = celeba_df.drop('Unnamed: 0', axis = 1)
+celeba_df.head()
+
+celeba_df.info()
+
+celeba_df.value_counts('gender')
+
+celeba_df.value_counts('smiling')
+
+## Cartoon_set labels.scv data
+
+cartoon_df = pd.read_csv('./datasets/cartoon_set/labels.csv')
+cartoon_df = cartoon_df.drop('Unnamed: 0', axis = 1)
+cartoon_df.head()
+
+cartoon_df.info()
+
+cartoon_df.isnull().sum()
+
+cartoon_df.value_counts('face_shape')
+
+cartoon_df.value_counts('eye_color')
+
+# Import Required Libraries
+
+# Task A1
+
+import
+# Task A2
+
+# Task B1
+
+# Task B2
 
